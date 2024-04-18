e6a1d72e344001efe7294730f2d2962b27e79cde
pi-josh <joshua.macatunao007@gmail.com>
2024-04-18 13:19:09 +0800

    Write a program which prints the number of minutes in a year

---
+++ Introduction to Programming/Part 1/minutes_in_a_year.py
@@ -0,0 +1,11 @@
+"""
+Programming exercise:
+Minutes in a year
+
+Please write a program which prints out the number of minutes in a
+year. Use Python code to perform the calculation, as in the previous
+code example.
+"""
+# Write your solution here
+print("Minutes in a year:")
+print(365 * 24 * 60)    # 365 days, 24 hours in each day, 60 minutes per hour
