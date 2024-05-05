fe9d9aaeee56bf4cdde78ee982c4315b64d3fd12
pi-josh <joshua.macatunao007@gmail.com>
2024-05-06 01:35:41 +0800

    finish part 4.1. visual studio code

--- tmcdata/mooc-programming-24/part04-01_hello_visual_studio_code/src/hello_visual_studio_code.py
+++ tmcdata/mooc-programming-24/part04-01_hello_visual_studio_code/src/hello_visual_studio_code.py
@@ -1,1 +1,12 @@
 # Write your solution here
+while True:
+    editor = input("Editor: ").lower()
+    
+    if editor == "visual studio code":
+        print("an excellent choice!")
+        break
+    elif editor == 'word' or editor == 'notepad':
+        print("awful")
+        continue
+    else:
+        print("not good")
