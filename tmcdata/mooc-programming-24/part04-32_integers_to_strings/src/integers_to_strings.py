888e07279f3b9f48947992d49ddb3f318975cb5d
pi-josh <joshua.macatunao007@gmail.com>
2024-05-12 10:42:00 +0800

    finish 4.5. print statement formatting

--- tmcdata/mooc-programming-24/part04-32_integers_to_strings/src/integers_to_strings.py
+++ tmcdata/mooc-programming-24/part04-32_integers_to_strings/src/integers_to_strings.py
@@ -1,1 +1,15 @@
 # Write your solution here
+def formatted(my_list: list) -> list:
+    new_list = []
+    
+    for num in my_list:
+        new_list.append(f"{num:.2f}")
+    return new_list
+
+def main():
+    my_list = [1.234, 0.3333, 0.11111, 3.446]
+    new_list = formatted(my_list)
+    print(new_list)
+    
+if __name__ == "__main__":
+    main()
