9955ebc4804e8ca5e4270bb03e61c2cd331cfd06
pi-josh <joshua.macatunao007@gmail.com>
2024-05-30 17:32:25 +0800

    finish 6.2. writing files

--- tmcdata/mooc-programming-24/part06-16_dictionary_file/src/dictionary_file.py
+++ tmcdata/mooc-programming-24/part06-16_dictionary_file/src/dictionary_file.py
@@ -1,1 +1,43 @@
 # Write your solution here
+filename = "dictionary.txt"
+dictionary = {}
+
+with open(filename) as file:
+        for line in file:
+            finnish, english = line.strip().split(";")
+            dictionary[finnish] = english
+            
+            
+def add_word():
+    finnish = input("The word in Finnish: ")
+    english = input("The word in English: ")
+    dictionary[finnish] = english
+    print("Dictionary entry added")
+    
+    with open(filename, "w") as file:
+        for finnish, english in dictionary.items():
+            line = f"{finnish};{english}\n"
+            file.write(line)
+
+
+def search_word():
+    search_term = input("Search term: ")
+    
+    for finnish, english in dictionary.items():
+        line = f"{finnish} - {english}"
+        if search_term in line:
+            print(line)
+
+
+while True:
+    print("1 - Add word, 2 - Search, 3 - Quit")
+    function = input("Function: ")
+    
+    match function:
+        case '1':
+            add_word()
+        case '2':
+            search_word()
+        case '3':
+            print("Bye!")
+            break
