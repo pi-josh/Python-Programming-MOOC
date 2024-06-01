5a3220839a52055846e5560b8fedb2cf22af0fcf
pi-josh <joshua.macatunao007@gmail.com>
2024-06-01 10:20:08 +0800

    finish 6.3. handling errors

--- tmcdata/mooc-programming-24/part06-17_read_input/src/read_input.py
+++ tmcdata/mooc-programming-24/part06-17_read_input/src/read_input.py
@@ -1,1 +1,16 @@
 # Write your solution here
+def read_input(prompt: str, start: int, end: int) -> int:
+    while True:
+        try:
+            number = int(input(prompt))
+            if number in range(start, end+1):
+                return number
+        except ValueError:
+            pass
+        
+        print(f"You must type in an integer between {start} and {end}")
+        
+
+if __name__ == "__main__":
+    number = read_input("Please type in a number: ", 5, 10)
+    print("You typed in:", number)
--- tmcdata/mooc-programming-24/part06-18_parameter_validation/src/parameter_validation.py
+++ tmcdata/mooc-programming-24/part06-18_parameter_validation/src/parameter_validation.py
@@ -1,1 +1,14 @@
 # Write your solution here
+def new_person(name: str, age: int) -> tuple:
+    length = len(name)
+    words = name.strip().split()
+    if not (length > 0 and length <= 40) or \
+            len(words) < 2 or \
+        not (age >= 0 and age <= 150):
+            raise ValueError
+    
+    return (name, age)
+
+
+if __name__ == "__main__":
+    person = new_person('Andrew', 32)
