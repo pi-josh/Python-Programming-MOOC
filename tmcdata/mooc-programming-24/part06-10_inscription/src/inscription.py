dbceaace47c3f783bcb00e95d1cce6c622920bb5
pi-josh <joshua.macatunao007@gmail.com>
2024-06-02 11:36:58 +0800

    files for part 7 in introduction to programming

--- tmcdata/mooc-programming-24/part06-10_inscription/src/inscription.py
+++
@@ -1,7 +0,0 @@
-# Write your solution here
-name = input("Whom should I sign this to: ")
-filename = input("Where shall I save it: ")
-
-with open(filename, "w") as file:
-    file.write(f"Hi {name}, we hope you enjoy learning Python with us! Best, Mooc.fi Team\n")
-    
