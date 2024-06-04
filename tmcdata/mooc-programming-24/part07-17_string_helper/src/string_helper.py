0afc9a532ee305d2d536b31bc4906cbebecdc48c
pi-josh <joshua.macatunao007@gmail.com>
2024-06-04 12:03:20 +0800

    finish 7.5. creating your own modules

--- tmcdata/mooc-programming-24/part07-17_string_helper/src/string_helper.py
+++ tmcdata/mooc-programming-24/part07-17_string_helper/src/string_helper.py
@@ -1,1 +1,21 @@
-# Write your solution here
+# Write your solution h
+def change_case(orig_string: str) -> str:
+    return orig_string.swapcase()
+
+
+def split_in_half(orig_string: str) -> str:
+    length = len(orig_string)
+    half = length // 2
+    first_half, second_half = orig_string[:half], orig_string[half:]
+    return first_half, second_half
+
+
+def remove_special_characters(orig_string: str) -> str:
+    return "".join([character for character in orig_string if character.isalnum() or character.isspace()])
+
+
+if __name__ == "__main__":
+    print(change_case("hEllO"))
+    print(split_in_half("hello"))
+    print(split_in_half("hellos"))
+    print(remove_special_characters("ed1§wo r!!!??s8876"))
